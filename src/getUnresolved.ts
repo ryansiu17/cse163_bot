4b12c1f4749a61d8d4267b5575bb9ce4a8b8f17b
Ryan Siu <ryansiu17@gmail.com>
2020-03-28 03:12:11 -0700

    added link and user data

--- src/getUnresolved.ts
+++ src/getUnresolved.ts
@@ -1,50 +1,73 @@
 export default async function(req, res, slack, token) {
   // get channel id from req body
   const { body } = req;
+
   // get history of channel
   const history = await slack.conversations.history({
     channel: body.channel_id,
     token: token
   });
-  console.log(body.channel_id);
-  console.log(history);
 
   // array of question blocks w/ links
-
-  const blocks = [
-    {
-      type: "section",
-      text: {
-        type: "mrkdwn",
-        text: "*Paul Pham*\nBig question with lots of text"
-      },
-      accessory: {
-        type: "button",
-        text: {
-          type: "plain_text",
-          emoji: true,
-          text: "Visit"
-        },
-        value: "click_me_123"
-      }
-    }
-  ];
+  let blocks = [];
 
   // for each message in history
   //    check if message has unresolved emoji and NO resolved emoji
   //    append to blocks array
-
   if (history.ok && history.messages) {
     for (const message of history.messages) {
       if (message.reactions) {
-        message.reactions.indexOf("emoji");
+        let emojis = [];
+        for (const reaction of message.reactions) {
+          emojis.push(reaction.name);
+        }
+        if (emojis.includes("x") && !emojis.includes("white_check_mark")) {
+          const question = message.text;
+          const poster = message.user;
+          const messageTs = message.ts;
+          // get user's name
+          const userInfo = await slack.users.info({
+            user: poster
+          });
+          const name = userInfo.ok ? userInfo.user.real_name : "No Name";
+          // get link to the chat
+          const linkInfo = await slack.chat.getPermalink({
+            channel: body.channel_id,
+            message_ts: messageTs
+          });
+          const link = linkInfo.ok ? linkInfo.permalink : "No Link";
+          // append all this data to output blocks
+          blocks.push({
+            type: "section",
+            fields: [
+              {
+                type: "mrkdwn",
+                text: `${name}*\n${question}`
+              },
+              {
+                type: "mrkdwn",
+                text: `*Time*\n<${link}|Visit>`
+              }
+            ]
+          });
+        }
       }
     }
   }
 
+  if (blocks.length === 0) {
+    blocks.push({
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: `No Unresolved Posts!`
+      }
+    });
+  }
+
   res
     .json({
-      text: `TodoBot Says:`,
+      text: `Returned unresolved questions`,
       response_type: "ephemeral",
       blocks: [
         {
