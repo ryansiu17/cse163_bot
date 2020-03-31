c8409ef8ccbf330ddefaface60f7cc2f37db05d3
Ryan Siu <ryansiu17@gmail.com>
2020-03-30 18:08:28 -0700

    added verification func

--- out/getUnresolved.js
+++ out/getUnresolved.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 async function default_1(req, res, slack, token) {
+    // TODO: MAKE BOT ADD SELF TO CHANNEL
     res
         .json({
         text: `Gathering Unresolved Posts:`,
@@ -10,7 +11,7 @@ async function default_1(req, res, slack, token) {
                 type: "section",
                 text: {
                     type: "mrkdwn",
-                    text: `*Gathering Unresolved Posts*\nPlease Wait... :smile: \n`
+                    text: `*Gathering Unresolved Posts*\nPlease Wait... :smile: \nIf I don't return soon, make sure I'm added to the channel!`
                 }
             }
         ]
@@ -19,77 +20,93 @@ async function default_1(req, res, slack, token) {
         .end();
     // get channel id from req body
     const { body } = req;
-    const responseUrl = body.response_url;
     // get history of channel
     const history = await slack.conversations.history({
         channel: body.channel_id,
         token: token
     });
-    // array of question blocks w/ links
-    let blocks = [];
-    // for each message in history
-    //    check if message has unresolved emoji and NO resolved emoji
-    //    append to blocks array
+    // array of unformatted results (no username or permalink yet)
+    let unformatted = [];
     if (history.ok && history.messages) {
+        // for each message in channel history
         for (const message of history.messages) {
+            // if this message has the correct emojis
             if (message.reactions) {
                 let emojis = [];
-                let reactCount = 0;
+                let react_count = 0;
                 for (const reaction of message.reactions) {
                     emojis.push(reaction.name);
-                    reactCount += reaction.count;
+                    react_count += reaction.count;
                 }
                 if (emojis.includes("x") && !emojis.includes("white_check_mark")) {
                     const reacts = emojis.map(x => {
                         return { type: "mrkdwn", text: `:${x}:` };
                     });
-                    const question = message.text;
-                    const userId = message.user;
-                    const messageTs = message.ts;
-                    // get user's name
-                    const userInfo = await slack.users.info({
-                        user: userId
-                    });
-                    const name = userInfo.ok ? userInfo.user.real_name : "No Name";
-                    // get link to the chat
-                    const linkInfo = await slack.chat.getPermalink({
-                        channel: body.channel_id,
-                        message_ts: messageTs
-                    });
-                    const link = linkInfo.ok ? linkInfo.permalink : "No Link";
-                    // append all this data to output blocks
-                    blocks.unshift(
-                    // Header w/ name and question + date and visit button
-                    {
-                        type: "section",
-                        fields: [
-                            {
-                                type: "mrkdwn",
-                                text: `*${name}*\n${question}`
-                            },
-                            {
-                                type: "mrkdwn",
-                                text: `<!date^${Math.floor(messageTs)}^{date} at {time}|Unable to get Timestamp>\n<${link}|Visit>`
-                            }
-                        ]
-                    }, 
-                    // reaction list
-                    {
-                        type: "context",
-                        elements: [
-                            ...reacts,
-                            {
-                                type: "plain_text",
-                                emoji: true,
-                                text: `${reactCount} React${reactCount != 1 ? "s" : ""}`
-                            }
-                        ]
-                    }, 
-                    // divider :)
-                    {
-                        type: "divider"
+                    unformatted.push({
+                        question: message.text,
+                        user_id: message.user,
+                        message_ts: message.ts,
+                        reacts: reacts,
+                        react_count: react_count
                     });
                 }
+            }
+        }
+        const result_user_data = unformatted.map(({ user_id }) => {
+            return slack.users.info({
+                user: user_id
+            });
+        });
+        const result_link_data = unformatted.map(({ message_ts }) => {
+            return slack.chat.getPermalink({
+                channel: body.channel_id,
+                message_ts: message_ts
+            });
+        });
+        const user_data = await Promise.all(result_user_data);
+        const link_data = await Promise.all(result_link_data);
+        let blocks = [];
+        let decorator = "";
+        // append all this data to output blocks
+        for (let i = 0; i < unformatted.length; i++) {
+            const name = user_data[i].ok ? user_data[i].user.real_name : "No Name";
+            const link = link_data[i].ok ? link_data[i].permalink : "No Link";
+            if (blocks.length > 48) {
+                decorator = "\n*OVER 15 UNRESOLVED. PLEASE RESOLVE AND REQUERY*";
+                break;
+            }
+            else {
+                blocks.unshift(
+                // Header w/ name and question + date and visit button
+                {
+                    type: "section",
+                    fields: [
+                        {
+                            type: "mrkdwn",
+                            text: `*${name}*\n${unformatted[i].question}`
+                        },
+                        {
+                            type: "mrkdwn",
+                            text: `<!date^${Math.floor(unformatted[i].message_ts)}^{date} at {time}|Unable to get Timestamp>\n<${link}|Visit>`
+                        }
+                    ]
+                }, 
+                // reaction list
+                {
+                    type: "context",
+                    elements: [
+                        ...unformatted[i].reacts,
+                        {
+                            type: "plain_text",
+                            emoji: true,
+                            text: `${unformatted[i].react_count} React${unformatted[i].react_count != 1 ? "s" : ""}`
+                        }
+                    ]
+                }, 
+                // divider :)
+                {
+                    type: "divider"
+                });
             }
         }
         if (blocks.length === 0) {
@@ -101,53 +118,19 @@ async function default_1(req, res, slack, token) {
                 }
             });
         }
-        const output = {
-            text: `Returned unresolved posts`,
-            blocks: [
-                {
-                    type: "section",
-                    text: {
-                        type: "mrkdwn",
-                        text: "*Unresolved Posts _(Oldest to Newest)_:*"
-                    }
-                },
-                {
-                    type: "divider"
-                },
-                ...blocks
-            ]
-        };
-        let outputBlocks = [];
-        if ([...blocks].length > 48) {
-            outputBlocks = [
-                {
-                    type: "section",
-                    text: {
-                        type: "mrkdwn",
-                        text: "*Unresolved Posts _(Oldest to Newest)_*:\n>*OVER 16 UNRESOLVED POSTS, REQUERY AFTER RESOLVING A FEW*"
-                    }
-                },
-                {
-                    type: "divider"
-                },
-                ...blocks
-            ].slice(0, 47);
-        }
-        else {
-            outputBlocks = [
-                {
-                    type: "section",
-                    text: {
-                        type: "mrkdwn",
-                        text: "*Unresolved Posts _(Oldest to Newest)_:*"
-                    }
-                },
-                {
-                    type: "divider"
-                },
-                ...blocks
-            ];
-        }
+        let outputBlocks = [
+            {
+                type: "section",
+                text: {
+                    type: "mrkdwn",
+                    text: "*Unresolved Posts _(Oldest to Newest)_:*" + decorator
+                }
+            },
+            {
+                type: "divider"
+            },
+            ...blocks
+        ];
         slack.chat.postEphemeral({
             text: "Unresolved Posts (Spliced)",
             channel: body.channel_id,
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ require("dotenv").config();
 import { WebClient } from "@slack/web-api";
 import express from "express";
 import bodyParser from "body-parser";
+import * as crypto from "crypto";
+import * as tsscmp from "tsscmp";
+
+import getUnresolved from "./getUnresolved";
 
 // constants
 const port = 4000;
@@ -10,11 +14,24 @@ const slackToken = process.env.SLACK_TOKEN;
 const userToken = process.env.USER_TOKEN;
 //const endpoint = "http://27c5b6da.ngrok.io";
 
-import getUnresolved from "./getUnresolved";
+function legitSlackRequest(req) {
+  // Your signing secret
+  const slackSigningSecret = process.env.SIGNING_SECRET;
 
-// make sure we have token
-if (!slackToken) {
-  throw new Error("No Slack token provided");
+  // Grab the signature and timestamp from the headers
+  const requestSignature = req.headers["x-slack-signature"] as string;
+  const requestTimestamp = req.headers["x-slack-request-timestamp"];
+
+  // Create the HMAC
+  const hmac = crypto.createHmac("sha256", slackSigningSecret);
+
+  // Update it with the Slack Request
+  const [version, hash] = requestSignature.split("=");
+  const base = `${version}:${requestTimestamp}:${JSON.stringify(req.body)}`;
+  hmac.update(base);
+
+  // Returns true if it matches
+  return tsscmp(hash, hmac.digest("hex"));
 }
 
 // make app and webclient
@@ -25,7 +42,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post(`/getUnresolved`, (req, res) => {
-  getUnresolved(req, res, slack, userToken);
+  const legit = legitSlackRequest(req);
+  if (!legit) {
+    res.status(403).send("Nice try buddy. Slack signature mismatch.");
+  } else {
+    getUnresolved(req, res, slack, userToken);
+  }
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
